c70991f1a92d93a9f877919ee265139d75035f94
YimnLu <yimnlu@qq.com>
2020-11-15 00:46:24 +0800

    aml

--- AML/src/main/java/com/yimnlu/AML/executor/DICT.java
+++ AML/src/main/java/com/yimnlu/AML/executor/DICT.java
@@ -1,7 +1,9 @@
 package com.yimnlu.AML.executor;
 
+import com.yimnlu.AML.executor.staticReturn.TodayWorkDate;
+
 public class DICT {
-    public static final String DEFAULT_WORKDATE = "DEFAULT_WORKDATE";
+    public static final String DEFAULT_WORKDATE = "00000000";
     public static final String DEFAULT_DUPLICATE_WORKDATE = "Turn default '00000000' to Today.";
     public static final String DEFAULT_DEPART_ID = "10385194";
     public static final String DEFAULT_DUPLICATE_DEPART_ID = "Turn default '00000000' to '10385194";
